315827fd874d3a9c021b8c26d5c182a6df951539
Ivan Janjić <wannjanjic@gmail.com>
2024-02-15 23:34:05 +0100

    first commit

---
+++ src/main/scala/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,24 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+
+  // VALS ARE IMMUTABLE
+
+  // COMPILER can infer types
+  val aString: String = "hello"
+  val anotherString = "goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 5123456454235453L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable: Int = 4
+  aVariable = 5 // side effects
+}
